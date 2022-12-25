d1f9de6bea3248a31b291a948b253890b161ef36
Rangha-Vardhan-1552 <rangavardhan1908@gmail.com>
2022-12-25 02:47:28 -0500

    Adding UI to TODO-App

--- migrations/20221225072917-create-todo.js
+++ migrations/20221225072917-create-todo.js
@@ -2,14 +2,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('todos', {
+    await queryInterface.createTable('Todos', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      title: {
+      tite: {
         type: Sequelize.STRING
       },
       dueDate: {
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('todos');
+    await queryInterface.dropTable('Todos');
   }
 };
